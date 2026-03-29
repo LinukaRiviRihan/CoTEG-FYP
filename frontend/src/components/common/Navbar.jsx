0802b43044ef82d7c8854df9c0366ed89a0e6a4c
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2026-03-29 13:50:03 +0530

    feat: enhanced user selection of moments for test the predictions

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <h1 className="text-xl md:text-2xl font-bold tracking-wide text-white">
           CoTEG
         </h1>
-        <h2 className="mt-1 md:mt-2 text-sm md:text-lg text-gray-300 max-w-md md:max-w-none leading-tight">
+        <h2 className="mt-1 md:mt-2 text-sm md:text-lg text-gray-300 italic max-w-md md:max-w-none leading-tight">
           A Hybrid T-GCN Model for Multi-Label Emotion Detection with Emotion
           Correlation Modeling
         </h2>
